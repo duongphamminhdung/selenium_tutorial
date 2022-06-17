260dd926f07aa9f31a97f812b451a6e2a5a4042b
Duong Pham Minh Dung <duongphamminhdung@gmail.con>
2022-06-17 23:49:17 +0700

    try1.py on mac

---
+++ try1.py
@@ -0,0 +1,15 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--incognito")
+chrome_options.add_argument("--window-size=1920x1080")
+
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="path_to_chromedriver")
+url = input('url? ')
+driver.get(url)
+time.sleep(5)
+
+contentBox = driver.find_element_by_class_name('content-box')
+print(contentBox)
