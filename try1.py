b39da6ec202a93880a61a118dc9c25a3afe1717d
Duong Pham Minh Dung <duongphamminhdung@gmail.con>
2022-06-18 00:04:39 +0700

    update try1.py

--- try1.py
+++ try1.py
@@ -7,9 +7,12 @@ chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--window-size=1920x1080")
 
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/Users/duongphamminhdung/Documents/GitHub/chromedriver")
-url = input('url? ')
+# url = input('url? ')
+url = 'https://sangtacviet.me/truyen/faloo/1/566332/1/'
 driver.get(url)
 time.sleep(5)
 
-contentBox = driver.find_element_by_class_name('content-box')
+print("SLEPT")
+contentBox = driver.find_element(By.CLASS_NAME,value= 'contentbox')
+print("FOUND")
 print(contentBox)
