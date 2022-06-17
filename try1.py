947036a052a58faa76e462db9873ddc14d8fb8aa
Duong Pham Minh Dung <duongphamminhdung@gmail.con>
2022-06-17 23:50:06 +0700

    created macos branch

--- try1.py
+++ try1.py
@@ -6,7 +6,7 @@ chrome_options = Options()
 chrome_options.add_argument("--incognito")
 chrome_options.add_argument("--window-size=1920x1080")
 
-driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="path_to_chromedriver")
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path="/Users/duongphamminhdung/Documents/GitHub/chromedriver")
 url = input('url? ')
 driver.get(url)
 time.sleep(5)
